7b9fb39e760b24d7fff524fe48bfb4efdc261e4f
mitra97 <rony.mitra1234@gmail.com>
2017-12-04 20:54:37 -0600

    Markers don't work

---
+++ source/components/Map/Container.jsx
@@ -0,0 +1,46 @@
+import logo2 from "./Marker.png"
+import { Link } from 'react-router-dom'
+import GoogleMapReact from 'google-map-react'
+import React, { Component } from 'react'
+
+
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'Item';
+        this.state = {
+            hover: false
+        };
+    }
+    mouseOver() {
+        this.setState({hover: true});
+
+    }
+    mouseOut() {
+        this.setState({hover: false});
+    }
+    render() {
+      const { item, i } = this.props;
+        
+            if(this.state.hover == true)
+            {
+              return (
+              <div id="parent-element">
+                <div id="child-element">{this.props.data}</div>
+                <img src = {logo2} width = "50px" height = "50px" />
+              </div>
+              
+              );
+            }
+            else
+            {
+              return (
+              <div ><img src = {logo2} width = "50px" height = "50px" /></div>
+              );
+            }
+        
+    }
+}
+
+
+export default Item;
--- source/components/Map/Map.jsx
+++ source/components/Map/Map.jsx
@@ -2,53 +2,85 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import GoogleMapReact from 'google-map-react'
 import marker from './Marker.png'
+import Item from './Container.jsx'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+import { compose, withProps, withStateHandlers } from "recompose"
 
-const AnyReactComponent = ({ image }) => <div ><img src = {marker} /></div>;
+
+const AnyReactComponent = ({ image }) => <div ><Item/></div>;
 
 
-var markers = [];
-
-markers.push([41.8781, -87.6298, marker]);
-markers.push([41.9781, -87.6298, marker])
-
-var lat_col = markers.map(function(value,index) { return value[0]; });
-var lng_col = markers.map(function(value,index) { return value[1]; });
-var img_col = markers.map(function(value,index) { return value[2]; });
 class Map extends Component {
   constructor(props){
     super(props);
-    // this.props.data will have all the data
   }
 
   render() {
-  	var listObjects = []
-  	for(var i = 0; i < lat_col.length; i++)
-      {
-      	listObjects.push(
-        <AnyReactComponent
-          lat={lat_col[i]}
-          lng = {lng_col[i]}
-          image = {img_col[i]}
-        />)
+    var listObjects = []
+    for(var i = 0; i < this.props.data.length; i++)
+    {
+      const marker = compose(
+        withStateHandlers(() => ({
+          isOpen: false,
+        }), {
+          onToggleOpen: ({ isOpen }) => () => ({
+            isOpen: !isOpen,
+          })
+        }),
+        withScriptjs,
+        withGoogleMap
+      )(props =>
 
-      }
-     console.log(listObjects)
-    return (
-      <GoogleMapReact
-      	bootstrapURLKeys={{key: 'AIzaSyAsY60PGtxjEuHAS4KsRrfjirl-piCayYk'}}
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-        style={{height: '300px'}}
+              <Marker
+        position={{ lat:this.props.data[i]['latitude'] , lng:this.props.data[i]['longitude'] }}
+        onClick={props.onToggleOpen}
+    >
+      {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+      <div> {this.props.data} </div>
+      </InfoWindow>}
+    </Marker>);
+
+      listObjects.push(marker)
+        
+
+    }
+
+    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+      <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: 41.8781, lng: -87.6298 }}
       >
-      {listObjects}
-      </GoogleMapReact>
+      {props.isMarkerShown && listObjects}
+      </GoogleMap>
+      ))
+      return (
+      <MyMapComponent
+      isMarkerShown
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAsY60PGtxjEuHAS4KsRrfjirl-piCayYk&v=3.exp&libraries=geometry,drawing,places"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `100%` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+      />
 
-    );
-  }
-}
-Map.defaultProps = {
-  center: {lat: 41.8781, lng: -87.6298},
-  zoom: 15
-};
+      )
+      // return (
+      //   {withGoogleMap((this.props) =>
+        //   <GoogleMap
+        //     bootstrapURLKeys={{key: 'AIzaSyAsY60PGtxjEuHAS4KsRrfjirl-piCayYk'}}
+        //     defaultCenter={this.props.center}
+        //     defaultZoom={this.props.zoom}
+        //     style={{height: '300px'}}
+        //   >
+        //   {listObjects}
+        //   </GoogleMap>)}
 
-export default Map;
+        // ));
+      }
+    }
+    Map.defaultProps = {
+      center: {lat: 41.8781, lng: -87.6298},
+      zoom: 15
+    };
+
+    export default Map;
+
